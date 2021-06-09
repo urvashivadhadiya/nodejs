9927b4ae7edca476c6e28f5c59e96c54aef511d0
D3@D53C71 <urvashivadhadiya@gmail.com>
2021-06-09 16:08:24 +0530

    httpserver

--- httpserver.js
+++ httpserver.js
@@ -1,1 +1,13 @@
-v
+const http = require("http")
+const hostname = '127.0.0.1'
+const port = 7777;
+
+const server = http.createServer((req, res) => {
+    // res.statusCode = 200;
+    //  res.setHeader('Content-Type', 'text/plain');
+    //  res.writeHead(600, { 'Content-Type': 'text/plain' });
+    res.end("welcome to http server");
+})
+server.listen(port, hostname, () => {
+    console.log(`server is running at http://${hostname}:${port}/`);
+})
